fb8838183f4025583f5b3482b3f21af5099ae310
Nonviens12 <115369029+Nonviens12@users.noreply.github.com>
2022-11-27 12:43:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+class Hero:
+    def __init__(self, name, BladePower, health):
+        self.name = name
+        self.BladePower = BladePower
+        self.health = health
+        print(f"Пришел новый столп {self.name}")
+
+    def attack(self):
+        print(f"{self.name} атаковал на {BladePower}")
+        SpecialPunch = (BladePower)*1.5
+        Punch = BladePower
+
+
+
+
+name = input("Введите имя первого героя: ")
+name2 = input("Введите имя второго героя: ")
+
+
+
+def fighting(name, name2):
+    input_text = f"атака героя {self.name}"
+    while input_text != "Бой окончен":
+        print(f"{name} атакует(Выбирете атаку): ")
+        print()
+
+
+
+
+
+Rengoku = Hero("Ренгоку", 50, 200)
+Shinobu = Hero("Шинобу", 30, 300)
+Giyu = Hero("Гию", 60, 170)
+fighting(Rengoku, Shinobu)
